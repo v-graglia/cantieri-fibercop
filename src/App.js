ff6e686dc63d9ae750b22783f9d4aaef08d28158
Vittorio Graglia <29167928+v-graglia@users.noreply.github.com>
2024-07-02 00:05:36 +0200

    Update Map app to show all data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
@@ -29,26 +29,37 @@ function App() {
   const [selectedProvince, setSelectedProvince] = useState('');
   const [selectedCity, setSelectedCity] = useState('');
   const [markers, setMarkers] = useState([]);
+  const [map, setMap] = useState(null);
+
+  const featureGroupRef = useRef();
 
   useEffect(() => {
-    axios.get('http://127.0.0.1:8001/data')
+    axios.get('https://raw.githubusercontent.com/v-graglia/cantieri-fibercop-data/main/data.json')
       .then(response => {
+        console.log('Data fetched successfully:', response.data);
         const data = response.data.data;
         setCityData(data);
         const provinces = [...new Set(data.map(item => item.PROVINCIA))].sort();
+        console.log('Provinces:', provinces);
         setProvinces(provinces);
         if (provinces.length > 0) {
           setSelectedProvince(provinces[0]);
+          console.log('Selected province set to:', provinces[0]);
         }
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
       });
   }, []);
 
   useEffect(() => {
     if (selectedProvince) {
       const filteredCities = [...new Set(cityData.filter(item => item.PROVINCIA === selectedProvince).map(item => item.COMUNE))].sort();
+      console.log('Filtered cities for province', selectedProvince, ':', filteredCities);
       setCities(filteredCities);
       if (filteredCities.length > 0) {
         setSelectedCity(filteredCities[0]);
+        console.log('Selected city set to:', filteredCities[0]);
       }
     }
   }, [selectedProvince, cityData]);
@@ -59,6 +70,20 @@ function App() {
       setMarkers(filteredData);
     }
   }, [selectedCity, cityData]);
+
+  // Effect to fit map bounds to markers when markers change
+  useEffect(() => {
+    if (map && markers.length > 0) {
+      const bounds = L.latLngBounds(markers.map(marker => [marker.LATITUDINE, marker.LONGITUDINE]));
+      map.flyToBounds(bounds, {
+        duration: 2, // Animation duration in seconds
+        // padding: [30, 30], // Padding in pixels (top, right, bottom, left),
+        maxZoom: 15
+
+      });
+
+    }
+  }, [map, markers]);
 
   const handleProvinceChange = (e) => {
     setSelectedProvince(e.target.value);
@@ -95,13 +120,31 @@ function App() {
           </div>
         </div>
 
-        <MapContainer center={[45.0, 7.6]} zoom={13} className="rounded shadow h-64" id="map">
+        <MapContainer 
+            ref={setMap} 
+            center={[45.0, 10]} 
+            zoom={13} 
+            className="rounded shadow h-64" 
+            id="map"
+        >
           <TileLayer
             attribution='© OpenStreetMap contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           {markers.map(marker => (
-            <Marker key={marker.ID_ELEMENTO} position={[marker.LATITUDINE, marker.LONGITUDINE]} icon={markerIcons[marker.STATO]}>
+            <Marker 
+                key={marker.ID_ELEMENTO} 
+                position={[marker.LATITUDINE, marker.LONGITUDINE]} 
+                icon={markerIcons[marker.STATO]}
+                eventHandlers={{
+                mouseover: () => {
+                    map.getContainer().style.cursor = 'pointer'; // Change cursor to pointer on marker hover
+                },
+                mouseout: () => {
+                    map.getContainer().style.cursor = ''; // Reset cursor on mouseout
+                }
+                }}
+            >
               <Popup>
                 <p><strong>Indirizzo:</strong> {marker.INDIRIZZO || '-'}</p>
                 <p><strong>Provincia:</strong> {marker.PROVINCIA || '-'}</p>
